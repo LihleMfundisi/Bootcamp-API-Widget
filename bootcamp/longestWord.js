9c523d37597c8ed050b4590463d09da853285175
LihleMfundisi <mfundisilihle@gmail.com>
2022-08-31 20:21:03 +0200

    First Commit

---
+++ bootcamp/longestWord.js
@@ -0,0 +1,13 @@
+export default function longestWord (word){
+     var sentence = word.split('');
+     let sum = 0
+     let longest = ''; 
+     for (let = 0; i < sentence.lenth; i++) {
+        let characters = sentece[i];
+        if (characters.lenth >= sum) {
+            sum characters.length; 
+            longest = characters; 
+        }
+     } 
+     return longest 
+}
